7fa813526326c90006a11e1bc05b52874eb809ef
casharine <casharine@gmail.com>
2022-02-07 20:18:51 +0900

    練習：条件分岐

---
+++ if.js
@@ -0,0 +1,20 @@
+// 基本
+
+    var age = 0;
+
+    if (age >= 20) {
+    console.log("adult");
+    } else if (age==0) {
+    console.log("baby");
+    } else {
+    console.log("child");
+}
+    
+// 論理演算併用
+    var age = 7;
+
+    if (age >= 5 && age < 10) {
+    console.log("5以上10未満");
+    } else {
+    console.log("それ以外");
+    }
