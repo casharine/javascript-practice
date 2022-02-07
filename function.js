41959af398875313c7b811047faa8092760fb69c
casharine <casharine@gmail.com>
2022-02-07 20:37:00 +0900

    練習：関数

---
+++ function.js
@@ -0,0 +1,17 @@
+// 基本
+
+function showMessage() {
+console.log("Hello World");
+}
+showMessage();
+
+
+// 関数の式代入（引数）
+
+  var sayHello = function(greeting) {
+    console.log(greeting);
+  }
+
+  sayHello("Good Morning");
+  sayHello("Good afternoon");
+  sayHello("Good evening");
