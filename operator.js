ee77a09b0d2534064c8a038628664ee9b9c919f2
casharine <casharine@gmail.com>
2022-02-07 20:12:52 +0900

    練習：演算子

---
+++ operator.js
@@ -0,0 +1,47 @@
+// 算術演算
+
+    var x = 10;
+    var y = 2;　
+
+    console.log(x + y);
+    console.log(x - y);
+    console.log(x * y);
+    console.log(x / y);
+    console.log(x % y);
+    
+
+// 関係演算子
+
+    var x = 10;
+    var y = 2;　
+
+    console.log(x > y);
+
+    var x = 10;
+    var y = 2;　
+
+    console.log(x == y);
+    console.log(x != y);
+
+// 論理演算子
+    var x = 8;
+    var y = 3;
+    　
+    console.log(x >= 5 && x <= 10);
+    console.log(y >= 5 && y <= 10);
+    
+    var x = 8;
+    var y = 3;
+
+    console.log(x == 3 || y == 3);
+    console.log(x == 1 || y == 1);　
+
+// 代入演算子
+    var x = 8;
+    var y = 8;
+
+    x++;
+    y--;    
+
+    console.log(x);
+    console.log(y);
